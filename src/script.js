df4e0779fcb50cb444e10240790087622bd6b8ff
Perfoys <prorok43battle@gmail.com>
2021-04-23 03:31:32 +0300

    Added simple template script

---
+++ src/script.js
@@ -0,0 +1,12 @@
+let caption = document.getElementsByClassName("ytp-caption-segment");
+
+if (caption == document.querySelector(":hover")) {
+    stopVideo();
+    alert(caption.innerHTML);
+}
+
+function stopVideo() {
+    const player = document.getElementsByClassName("video-stream html5-main-video");
+    player.pause();
+    player.currentTime = 0;
+}
